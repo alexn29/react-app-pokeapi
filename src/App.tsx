c50749a103b6b5fdb227f2c1f995bf1cd4f5174b
Alex Navarro <lexn29@gmail.com>
2024-03-24 10:34:50 -0600

    integrate query client provider and Pokemons component into App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import { MantineProvider } from '@mantine/core';
-import Layout from '@/components/Layout';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Layout from '@/shared/components/Layout';
+import Pokemons from '@/pages/Pokemons';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Layout>
-        <h1>Hello world</h1>
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Pokemons />
+        </Layout>
+      </QueryClientProvider>
     </MantineProvider>
   );
 }
