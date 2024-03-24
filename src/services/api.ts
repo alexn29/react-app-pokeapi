7a72edbcc974c89872fcd685c2846d37f125c7fd
Alex Navarro <lexn29@gmail.com>
2024-03-24 10:47:26 -0600

    add api service for fetching pokemon list and image

---
+++ src/services/api.ts
@@ -0,0 +1,21 @@
+import type { GetPokemonList, PokemonListResult } from '@/app-types/pokemon';
+
+const API_URL = 'https://pokeapi.co/api/v2';
+const SPRITE_URL =
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork';
+
+export async function getPokemonList({
+  page,
+  limit,
+}: GetPokemonList): Promise<PokemonListResult> {
+  const offset = (page - 1) * limit;
+
+  const response = await fetch(
+    `${API_URL}/pokemon?limit=${limit}&offset=${offset}`,
+  );
+  return response.json();
+}
+
+export function getPokemonImageURL(id: number) {
+  return `${SPRITE_URL}/${id}.png`;
+}
