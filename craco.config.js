c7927d07fd4eaf2257969ffffb2794ad1c2fd557
Alex Navarro <lexn29@gmail.com>
2024-03-24 10:22:26 -0600

    add SVG support to craco config

--- craco.config.js
+++ craco.config.js
@@ -5,5 +5,12 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+    rules: [
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      },
+    ],
   },
 };
