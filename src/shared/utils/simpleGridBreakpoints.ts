59b3ddd83b00fa6d9e745d4b2a7395dce0c50073
Alex Navarro <lexn29@gmail.com>
2024-03-24 10:44:45 -0600

    create breakpoints for SimpleGrid component

---
+++ src/shared/utils/simpleGridBreakpoints.ts
@@ -0,0 +1,7 @@
+import type { SimpleGridBreakpoint } from '@mantine/core';
+
+export const simpleGridBreakpoints: SimpleGridBreakpoint[] = [
+  { maxWidth: '62rem', cols: 3, spacing: 'md' },
+  { maxWidth: '48rem', cols: 2, spacing: 'sm' },
+  { maxWidth: '36rem', cols: 1, spacing: 'sm' },
+];
