743f658cd0b2c6436073f4b44d32383bbbd0ddfb
Alex Navarro <lexn29@gmail.com>
2024-03-25 01:36:45 -0600

    replace Card with UnstyledButton in Pokemon for improved click behavior

--- src/pages/Pokemons/components/Pokemon/Pokemon.tsx
+++ src/pages/Pokemons/components/Pokemon/Pokemon.tsx
@@ -1,26 +1,43 @@
-import { Card, Flex, Text, Image, createStyles } from '@mantine/core';
+import {
+  Box,
+  Flex,
+  Text,
+  Image,
+  createStyles,
+  UnstyledButton,
+} from '@mantine/core';
 import type { Pokemon as PokemonType } from '@/app-types/pokemon';
 import wave from '@/assets/wave.svg';
 
-const useStyles = createStyles(() => ({
+const useStyles = createStyles((theme) => ({
   card: {
+    backgroundColor: theme.white,
     backgroundImage: `url(${wave})`,
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'bottom center',
     backgroundSize: 'cover',
+    borderRadius: theme.radius.lg,
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: theme.colors.gray[4],
   },
 }));
 
 type PokemonProps = {
   data: PokemonType;
+  onClick: (id: number) => void;
 };
-export default function Pokemon({ data }: PokemonProps) {
+export default function Pokemon({ data, onClick }: PokemonProps) {
   const { classes } = useStyles();
   const { id, url, name } = data;
 
   return (
-    <Card key={id} radius='lg' withBorder className={classes.card}>
-      <Card.Section p='lg'>
+    <UnstyledButton
+      key={id}
+      className={classes.card}
+      onClick={() => onClick(id)}
+    >
+      <Box p='lg'>
         <Flex direction='column' justify='center' align='center'>
           <Image
             src={url}
@@ -34,7 +51,7 @@ export default function Pokemon({ data }: PokemonProps) {
             {name}
           </Text>
         </Flex>
-      </Card.Section>
-    </Card>
+      </Box>
+    </UnstyledButton>
   );
 }
