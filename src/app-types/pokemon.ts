2e46ebab2469934aa28c1cc5be88aec43b2b3f49
Alex Navarro <lexn29@gmail.com>
2024-03-25 01:28:30 -0600

    add type definition for PokemonDetails component

--- src/app-types/pokemon.ts
+++ src/app-types/pokemon.ts
@@ -18,3 +18,48 @@ export type Pokemon = {
   name: string;
   url: string;
 };
+
+type Stat = {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: string;
+    url: string;
+  };
+};
+
+type Type = {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+};
+
+export type PokemonDetails = {
+  id: number;
+  height: number;
+  name: string;
+  base_experience: number;
+  sprites: {
+    back_default: string;
+    back_shiny: string;
+    front_default: string;
+    front_shiny: string;
+    other: {
+      'official-artwork': {
+        front_default: string;
+        front_shiny: string;
+      };
+      showdown: {
+        back_default: string;
+        back_shiny: string;
+        front_default: string;
+        front_shiny: string;
+      };
+    };
+  };
+  stats: Stat[];
+  weight: number;
+  types: Type[];
+};
