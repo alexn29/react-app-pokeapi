9e2791996c494d635ea5ab2cb9c3f0766e298cea
Alex Navarro <lexn29@gmail.com>
2024-03-25 01:29:47 -0600

    define typeColors based on pokemon type

--- src/shared/utils/pokemonUtils.ts
+++ src/shared/utils/pokemonUtils.ts
@@ -1,6 +1,28 @@
+import { MantineColor } from '@mantine/core';
+
 export const getPokemonID = (url: string) => {
   const regex = /\/([^/]+)\/$/;
   const match = url.match(regex);
 
   return match ? Number(match[1]) : 0;
+};
+
+export const typeColors: Record<string, MantineColor> = {
+  normal: 'dark.3',
+  fire: 'red.5',
+  water: 'blue.5',
+  grass: 'green.5',
+  electric: 'yellow.5',
+  flying: 'blue.1',
+  bug: 'green.9',
+  poison: 'grape.9',
+  ground: 'orange.9',
+  rock: 'gray.5',
+  fighting: 'orange.3',
+  fairy: 'pink.5',
+  dragon: 'violet.9',
+  steel: 'grey.3',
+  ghost: '#fff',
+  ice: 'blue.1',
+  dark: 'dark.5',
 };
